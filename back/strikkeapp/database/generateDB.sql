8ea7c3d658d52a5bf78271260c41292bfc91a5d2
SkjalgS <ssl028@uit.no>
2024-01-31 13:01:00 +0100

    :card_file_box: sqlite database implemented

---
+++ back/strikkeapp/database/generateDB.sql
@@ -0,0 +1,15 @@
+-- Create the "userLogIn" table
+CREATE TABLE IF NOT EXISTS userLogIn (
+    userID INTEGER PRIMARY KEY,
+    userEmail TEXT,
+    userPwd TEXT
+);
+
+-- Create the "userInfo" table with a foreign key reference to "userLogIn"
+CREATE TABLE IF NOT EXISTS userInfo (
+    userID INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER CHECK (age > 0),
+    gender TEXT CHECK (gender IN ('male', 'female', 'non-binary')),
+    FOREIGN KEY (userID) REFERENCES userLogIn(userID)
+);
