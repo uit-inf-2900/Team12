fd935cd8d2e09025492ad299ab4dce9c8e2dd6b2
SkjalgS <ssl028@uit.no>
2024-01-31 14:35:58 +0100

    :bulb: Comments

--- back/strikkeapp/database/tests/testDB.sql
+++ back/strikkeapp/database/tests/testDB.sql
@@ -1,8 +1,11 @@
+-- Create new user entry
 INSERT INTO userLogIn(userID, userEmail, userPwd)
 VALUES(123, 'ssl028@uit.no', 'StikkingErKult');
 
+-- New user entry with same userID
 INSERT INTO userLogIn(userID, userEmail, userPwd)
 VALUES(123, 'xxx000@uit.no', 'StikkingErKult');
 
+-- New user entry with same userEmail
 INSERT INTO userLogIn(userID, userEmail, userPwd)
 VALUES(666, 'ssl028@uit.no', 'StikkingErKult');
