f400e86cd3e3221a4c140a2f49a3d940afc06f09
SkjalgS <ssl028@uit.no>
2024-02-01 12:17:08 +0100

    :bricks: Moved .sln so it now works

--- back/strikkeapp/Program.cs
+++ back/strikkeapp/Program.cs
@@ -1,8 +1,13 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using strikkeapp.services;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<IUserService, UserService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
