cd35cfb9b3c0fbcca0f528c553824d86ac568acb
SkjalgS <ssl028@uit.no>
2024-02-02 13:58:53 +0100

    :card_file_box::white_check_mark: Updated tests to match new database

--- back/strikkeapp/database/tests/testDB.sql
+++ back/strikkeapp/database/tests/testDB.sql
@@ -3,45 +3,42 @@ PRAGMA foreign_keys = ON;
 
 -- Tests for userLogIn table
 -- Create valid user entries
-INSERT INTO userLogIn(userID, userEmail, userPwd, userStatus)
-VALUES(123, 'ssl028@uit.no', 'StikkingErKult', 'verified');
+INSERT INTO userLogIn(userEmail, userPwd, userStatus)
+VALUES('ssl028@uit.no', 'StikkingErKult', 'verified');
 
-INSERT INTO userLogIn(userID, userEmail, userPwd, userStatus)
-VALUES(666, 'satan@hell.com', 'AllHailSatan', 'unverified');
+INSERT INTO userLogIn(userEmail, userPwd, userStatus)
+VALUES('satan@hell.com', 'AllHailSatan', 'unverified');
 
-INSERT INTO userLogIn(userID, userEmail, userPwd, userStatus)
-VALUES(55, 'student@uit.no', 'JegErStudent', 'verified');
+INSERT INTO userLogIn(userEmail, userPwd, userStatus)
+VALUES('student@uit.no', 'JegErStudent', 'verified');
 
 -- Tests for invalid user entries
--- New user entry with same userID
-INSERT INTO userLogIn(userID, userEmail, userPwd, userStatus)
-VALUES(123, 'xxx000@uit.no', 'StikkingErKult', 'verified');
+-- Attempt to insert a new user with the same userEmail
+INSERT INTO userLogIn(userEmail, userPwd, userStatus)
+VALUES('ssl028@uit.no', 'DifferentPassword', 'verified');
 
--- New user entry with same userEmail
-INSERT INTO userLogIn(userID, userEmail, userPwd, userStatus)
-VALUES(6000, 'ssl028@uit.no', 'StikkingErKult', 'unverified');
+-- Attempt to insert a new user with an invalid userStatus
+INSERT INTO userLogIn(userEmail, userPwd, userStatus)
+VALUES('invalid@uit.no', 'GreatPassword', 'invalid');
 
--- New user entry with invalid userStatus
-INSERT INTO userLogIn(userID, userEmail, userPwd, userStatus)
-VALUES(512, 'invalid@uit.no', 'GreatPassword', 'invalid');
 
 
 -- Tests for userInfo table
 --  Valid user entry
 INSERT INTO userInfo(userID, userFullName, userAge, userGender, userType)
-VALUES(123, 'Skjalg Alexander Slubowski', 22, 'male', 'admin');
+VALUES(1, 'Skjalg Alexander Slubowski', 22, 'male', 'admin');
 
 -- Tests for invalid user entries
 -- Invalid userID
 INSERT INTO userInfo(userID, userFullName, userAge, userGender, userType)
-VALUES(6969, 'Emilie Steen', 23, 'female', 'user');
+VALUES(6969, 'Invalid User', 23, 'female', 'user');
 
 -- Invalid gender
 INSERT INTO userInfo(userID, userFullName, userAge, userGender, userType)
-VALUES(666, 'Satan', 666, 'attack helicopter', 'user');
+VALUES(2, 'Satan', 666, 'attack helicopter', 'user');
 
 -- Invalid userType
 INSERT INTO userInfo(userID, userFullName, userAge, userGender, userType)
-VALUES(55, 'Student', 23, 'non-binary', 'student');
+VALUES(3, 'Student', 23, 'non-binary', 'student');
 
 -- Total errros should be 6
