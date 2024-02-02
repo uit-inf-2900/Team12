a7d9c0d993893397b616a7747ba6269822559455
SkjalgS <ssl028@uit.no>
2024-02-02 13:58:03 +0100

    :card_file_box: Autoincrement userID

--- back/strikkeapp/database/generateDB.sql
+++ back/strikkeapp/database/generateDB.sql
@@ -3,11 +3,12 @@ PRAGMA foreign_keys = ON;
 
 -- Create the "userLogIn" table
 CREATE TABLE IF NOT EXISTS userLogIn (
-    userID INTEGER PRIMARY KEY,
+    userID INTEGER PRIMARY KEY AUTOINCREMENT,
     userEmail TEXT UNIQUE,
     userPwd TEXT,
     userStatus TEXT CHECK (userStatus IN ('unverified', 'verified', 'banned', 'inactive'))
 );
+
 
 -- Create the "userInfo" table with a foreign key reference to "userLogIn"
 CREATE TABLE IF NOT EXISTS userInfo (
