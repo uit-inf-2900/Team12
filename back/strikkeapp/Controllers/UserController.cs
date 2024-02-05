c4d9031b3f29d83e390ea113444e791d7585e848
SkjalgS <ssl028@uit.no>
2024-02-05 15:58:14 +0100

    :sparkles: Userinformation will now be added to database

--- back/strikkeapp/Controllers/UserController.cs
+++ back/strikkeapp/Controllers/UserController.cs
@@ -17,34 +17,39 @@ public class UsersController : ControllerBase
     {
         public string? UserEmail { get; set; }
         public string? UserPwd { get; set; }
+        public string? UserFullName { get; set; }
+        public int UserDOB { get; set; }
+        public string? UserGender { get; set; }
+
+        // Check that request is valid
+        public bool requestOK()
+        {
+            return (!string.IsNullOrWhiteSpace(UserEmail) &&
+           !string.IsNullOrWhiteSpace(UserPwd) &&
+           !string.IsNullOrWhiteSpace(UserFullName) &&
+           !string.IsNullOrWhiteSpace(UserGender));
+        }
     }
+ 
 
-    [HttpGet]
-    [Route("/test")]
-    public bool Test()
-    {
-        return _userService.Test();
-    }
-
-
-    //[HttpGet(Route = "/login")]
-    //public
 
     [HttpPost]
     [Route("/createuser")]
     public IActionResult CreateUser([FromQuery] CreateUserRequest request)
     {
-        if (string.IsNullOrWhiteSpace(request.UserEmail) || string.IsNullOrWhiteSpace(request.UserPwd))
+        if (!request.requestOK())
         {
-            return BadRequest("Missing email or password.");
+            return BadRequest();
         }
 
-        var result = _userService.CreateUser(request.UserEmail, request.UserPwd);
+        var result = _userService.CreateUser(request.UserEmail, request.UserPwd, request.UserFullName, request.UserDOB, request.UserGender);
         if(result == -1)
         {
-            return StatusCode(500, "Unable to proccess user");
+            return BadRequest("Unable to proccess user");
+            //return StatusCode(500, "Unable to proccess user");
         }
 
+        //return Created(result);
         return Ok(result);
     }
 }
--- back/strikkeapp/Services/UserService.cs
+++ back/strikkeapp/Services/UserService.cs
@@ -5,15 +5,14 @@ namespace strikkeapp.services;
 
 public interface IUserService
 {
-	bool Test();
-	public int CreateUser(string userEmail, string userPwd);
+    public int CreateUser(string userEmail, string userPwd, string userFullName, int? userDOB, string userGender);
 }
 
 public class UserService : IUserService
 {
-	public UserService() 
-	{
-	}
+    public UserService()
+    {
+    }
 
     private readonly PasswordHasher<object> _passwordHasher = new PasswordHasher<object>();
 
@@ -23,54 +22,68 @@ public class UserService : IUserService
         return _passwordHasher.HashPassword(null!, password);
     }
 
-    public bool Test()
-	{
-		return true;
-	}
+    public int CreateUser(string userEmail, string userPwd, string userFullName, int? userDOB, string userGender)
+    {
+        // Hash password
+        var hashedPwd = HashPassword(userPwd);
 
-	public int CreateUser(string userEmail, string userPwd)
-	{
-		// Hash password
-		var hashedPwd = HashPassword(userPwd);
-
-		// Connection to database
+        // Connection to database
         string connectionString = "Data Source=database/userInfo.db;Version=3;";
 
-		// Try to insert user into database
-		try
-		{
-			using (var connection = new SQLiteConnection(connectionString))
-			{
-				connection.Open();
-				var command = connection.CreateCommand();
-				// Insert user into database and return the id of the new user
-				command.CommandText = @"
-					INSERT INTO userLogIn (userEmail, userPwd, userStatus) 
-					VALUES (@userEmail, @userPwd, @userStatus);
-					SELECT last_insert_rowid();";
-				command.Parameters.AddWithValue("@userEmail", userEmail);
-				command.Parameters.AddWithValue("@userPwd", hashedPwd);
-				command.Parameters.AddWithValue("@userStatus", "unverified");
 
-				// Execute the command, and get userID
-				var q_result = command.ExecuteScalar();
-				if(q_result != null)
-				{
-					return Convert.ToInt32(q_result);
-				}
-				// If the command fails, return -1
-				else
-				{
-					return -1;
-				}
-			}
-		}
+        // Try to insert user into database
+        try
+        {
+            int userId = -1; // Default value indicating failure
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.Transaction = transaction;
+                        command.CommandText = @"
+                            INSERT INTO userLogIn (userEmail, userPwd, userStatus) 
+                            VALUES (@userEmail, @userPwd, @userStatus);
+                            SELECT last_insert_rowid();";
+                        command.Parameters.AddWithValue("@userEmail", userEmail);
+                        command.Parameters.AddWithValue("@userPwd", hashedPwd);
+                        command.Parameters.AddWithValue("@userStatus", "unverified");
 
-		// If an error occurs, return -1
-        catch (Exception ex) 
-		{
-			Console.WriteLine($"An error occured when creating user: {ex.Message}");
-			return -1;
-		}
-	}
+                        var q_result = command.ExecuteScalar();
+                        if (q_result != null && int.TryParse(q_result.ToString(), out userId))
+                        {
+                            // Clear previous parameters and add new command
+                            command.Parameters.Clear();
+                            command.CommandText = @"
+                                INSERT INTO userDetails(userID, userFullName, userDateOfBirth, userGender, userType)
+                                VALUES(@userID, @userFullName, @userDateOfBirth, @userGender, @userType);";
+
+                            command.Parameters.AddWithValue("@userID", userId);
+                            command.Parameters.AddWithValue("@userFullName", userFullName);
+                            command.Parameters.AddWithValue("@userDateOfBirth", userDOB);
+                            command.Parameters.AddWithValue("@userGender", userGender);
+                            command.Parameters.AddWithValue("@userType", "user");
+
+                            command.ExecuteNonQuery();
+                            transaction.Commit(); // Commit only if everything is successful
+                        }
+                        else
+                        {
+                            transaction.Rollback(); // Rollback if the first insert fails
+                            userId = -1; // Ensure userId reflects failure
+                        }
+                    }
+                }
+            }
+            return userId;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred when creating user: {ex.Message}");
+            return -1;
+        }
+
+    }
 }
