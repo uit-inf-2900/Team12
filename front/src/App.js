6bc98f937cf678dc2f8de4e5830b46178c57dd67
SkjalgS <ssl028@uit.no>
2024-02-12 09:35:50 +0100

    :twisted_rightwards_arrows: Merge branch 'origin/LoginAndSignup' into Front&Back

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,59 @@
-import './App.css';
+import React, { useState } from 'react';
+import LogIn from './SignUp_LogIn/LogIn';
+import SignUp from './SignUp_LogIn/SignUp';
+import ResetPassword from './SignUp_LogIn/ResetPassword';
+import NavBar from './SignUp_LogIn/NavBar'
+import { BrowserRouter as Router,  Route, Routes } from "react-router-dom";
 
-function App() {
+import {About} from './SignUp_LogIn/pages/about'
+import {Home} from './SignUp_LogIn/pages/home'
+
+
+
+
+export default function App() {
+  const [showLogin, setShowLogin] = useState(true);
+  const [showResetPassword, setShowResetPassword] = useState(false);
+
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+    setShowResetPassword(false);
+  };
+  const handleForgotPasswordClick = () => {
+    setShowLogin(false);
+    setShowResetPassword(true);
+  };
+  const toggleSignup = () => {
+    setShowSignup(!showSignup);
+  };
+  const toggleLogin = () => {
+    setShowLogin(!showLogin);
+  };
+
+  {/*const router=createBrowserRouter(createRoutesFromElements(<Route path="/" element={<Root />}>
+  <Route path="dashboard" element={<Dashboard />} />
+  {/* ... etc.
+</Route>));*/}
+
+
   return (
-    <div className="App">
-      <h1>Strikke App</h1>
-
+    <div className="form-container">
+      {showLogin && !showResetPassword &&(
+        <LogIn toggleForm={toggleForm} onForgotPasswordClick={handleForgotPasswordClick} />
+      )}
+      {!showLogin && !showResetPassword &&(
+        <SignUp toggleForm={toggleForm} />
+      )}
+      {showResetPassword && (
+        <ResetPassword toggleForm={toggleForm} />
+      )}
+      {/* NavBar component */}
+      <NavBar toggleSignup={toggleForm} toggleLogin={toggleLogin} />
+      
 
     </div>
+
+
+  
   );
 }
-
-export default App;
---
+++ front/src/SignUp_LogIn/ResetPassword.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './Reg.css';
+import InputField from './InputField'; 
+
+
+const ResetPassword = ({ toggleForm }) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [isValidEmail, setIsValidEmail] = useState(false);
+
+    const onSubmit = (data) => {
+        console.log('Forgot Password Data', data);
+        const isEmailValid = /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/.test(data.email);
+        setIsValidEmail(isEmailValid);
+    };  
+
+    return (
+        <div className="box-container">
+            <div className="box light">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <h2>Reset password</h2>
+                    
+                    {/* Use the InputField component for email input */}
+                    <InputField
+                        type="text"
+                        placeholder="Email"
+                        register={register("email", {
+                            required: "Email is required.",
+                            pattern: {
+                                value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                                message: "Email is not valid.",
+                            },
+                        })}
+                        errors={errors.email}
+                    />
+                    
+                    {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+                    
+                    <div className='larger-text'>
+                        {isValidEmail && (
+                            <p href="#">If you have a user with us, a new password will be sent to your email.</p>
+                        )}
+                    </div>
+                    
+                    {!isValidEmail && (
+                        <div className='small-text'>
+                            <a href="#" onClick={toggleForm}>Log In</a>
+                        </div>
+                    )}
+                    
+                    <div className='purple'>
+                        {isValidEmail ? (
+                            <button type='submit' onClick={toggleForm}>Log In</button>
+                        ) : (
+                            <button type="submit">Reset Password</button>
+                        )}
+                    </div>
+                </form>
+            </div> 
+        </div>
+    );
+};
+
+export default ResetPassword;
