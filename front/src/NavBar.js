ef17f89e83037e0ce9ba882eb27e0e2098de1b0a
Sera Madeleine Elstad <sel063@uit.no>
2024-02-28 14:43:29 +0100

    still cleaning up css

---
+++ front/src/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+
+
+const NavBar = ({ isLoggedIn, handleLogout }) => {
+  isLoggedIn === true;
+  return (
+    <header className="header" id="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav-logo">**LOGO**</NavLink>
+        <ul className="nav-list">
+          <li className="nav-item"><NavLink to="/" className="nav-link">Home</NavLink></li>
+          {!isLoggedIn && (
+            <>
+          <li className="nav-item"><NavLink to="/about" className="nav-link">About</NavLink></li>
+          </>
+          )}
+          {isLoggedIn && (
+            <>
+          <li className="nav-item"><NavLink to="/recipes" className="nav-link">Oppskrifter</NavLink></li>
+          <li className="nav-item"><NavLink to="/stash" className="nav-link">Garnlager</NavLink></li>
+          </>
+          )}
+          <li className="nav-item"><NavLink to="/kontaktoss" className="nav-link">Kontakt oss</NavLink></li>
+          {/* Add more navigation items */}
+        </ul>
+        {isLoggedIn && (
+          <>
+        <div className="nav-items">
+          <NavLink to="/profile" className="nav-action">Profile</NavLink>
+          <NavLink to="/logout" className="nav-action">LogOut</NavLink>
+        </div>
+        </>
+        )}
+        {!isLoggedIn && (
+          <>
+        <div className="nav-items">
+          <NavLink to="/signup" className="nav-action">Sign Up</NavLink>
+          <NavLink to="/login" className="nav-action">Log In</NavLink>
+        </div>
+        </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
