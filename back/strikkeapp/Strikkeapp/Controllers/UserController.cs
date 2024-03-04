ba506e76adecb8e6ac5800c8c329682df5a3f0ae
Sera Madeleine Elstad <sel063@uit.no>
2024-03-04 11:59:36 +0100

    emty commit

---
+++ back/strikkeapp/Strikkeapp/Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using strikkeapp.services;
+
+namespace strikkeapp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _userService;
+    public UsersController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    public class CreateUserRequest
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPwd { get; set; } = string.Empty;
+        public string UserFullName { get; set; } = string.Empty;
+        public int UserDOB { get; set; }
+
+        // Check that request is valid
+        public bool requestOK()
+        {
+            return (!string.IsNullOrWhiteSpace(UserEmail) &&
+           !string.IsNullOrWhiteSpace(UserPwd) &&
+           !string.IsNullOrWhiteSpace(UserFullName));
+        }
+
+        public DateTime Dob2Dt ()
+        {
+            int year = UserDOB / 10000;
+            int month = (UserDOB / 100) % 100;
+            int day = UserDOB % 100;
+
+            DateTime dob = new DateTime(year, month, day);
+            return dob;
+        }
+    }
+
+    public class LogInUserRequest
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPwd { get; set; } = string.Empty;
+
+        public bool requestOk()
+        {
+            return (!string.IsNullOrWhiteSpace(UserEmail) &&
+                !string.IsNullOrWhiteSpace(UserPwd));
+        }
+    }
+
+
+
+    [HttpPost]
+    [Route("/createuser")]
+    public IActionResult CreateUser([FromBody] CreateUserRequest request)
+    {
+        if (!request.requestOK())
+        {
+            return BadRequest();
+        }
+
+        var result = _userService.CreateUser(request.UserEmail, request.UserPwd, request.UserFullName, request.Dob2Dt());
+        
+        if(!result.Success)
+        {
+            if(result.ErrorMessage == "Email already exsits")
+            {
+                return Conflict(result.ErrorMessage);
+            }
+
+            return BadRequest(result.ErrorMessage);
+        }
+
+        return Ok(result.UserId);
+    }
+
+    [HttpPost]
+    [Route("/login")]
+    public IActionResult LogInUser([FromBody] LogInUserRequest request)
+    {
+
+        var result = _userService.LogInUser(request.UserEmail, request.UserPwd);
+        if (!result.Success)
+        {
+            if (result.ErrorMessage == "Invalid login attempt" ||
+                result.ErrorMessage == "User is banned")
+            {
+                return Unauthorized(result.ErrorMessage);
+            }
+
+            return StatusCode(500, result.ErrorMessage);
+        }
+
+
+        return Ok(new {
+            UserID = result.UserId,
+             Token = result.Token});
+    }
+}
---
+++ back/strikkeapp/Strikkeapp/Data/Models/UserDbModel.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Strikkeapp.Data.Models
+{
+    public class UserLogIn
+    {
+        // Define UserLogIn schema
+        [Key]
+        public Guid UserId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; } = string.Empty;
+
+        [Required]
+        public string UserPwd { get; set; } = string.Empty;
+
+        [Required]
+        [CustomValidation(typeof(UserLogIn), nameof(ValidateUserStatus))]
+        public string UserStatus { get; set; } = "unverified";
+
+        // Validation for user status
+        public static ValidationResult ValidateUserStatus(string userStatus, ValidationContext context)
+        {
+            var validStatuses = new List<string> { "unverified", "verified", "banned", "inactive" };
+            if (validStatuses.Contains(userStatus))
+            {
+                return ValidationResult.Success!;
+            }
+            return new ValidationResult("Invalid status");
+        }
+    }
+
+    public class UserDetails
+    {
+        // Define UserDetails schema
+        [Key]
+        [ForeignKey("UserLogIn")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string UserFullName { get; set; } = string.Empty;
+
+        [Required]
+        // Consider using DateTime type
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [CustomValidation(typeof(UserDetails), nameof(ValidateUserType))]
+        public string UserType { get; set; } = "user";
+
+        // Validation for user type
+        public static ValidationResult ValidateUserType(string userType, ValidationContext context)
+        {
+            var validTypes = new List<string> { "admin", "user" };
+            if (validTypes.Contains(userType))
+            {
+                return ValidationResult.Success!;
+            }
+            return new ValidationResult("Invalid type");
+        }
+    }
+
+    public class KnittingRecipes
+    {
+        [Key]
+       
+        public Guid KnittingRecipeId { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [ForeignKey("UserLogIn")]
+        public Guid UserId { get; set;}
+
+        [Required]
+        public byte[]? RecipePDF { get; set;}
+    }
+}
