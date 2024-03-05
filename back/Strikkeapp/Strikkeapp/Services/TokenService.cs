1283698b87f12361cd75340624bc3d6e1e537d3e
SkjalgS <ssl028@uit.no>
2024-03-05 14:13:26 +0100

    Does not use ?

--- back/Strikkeapp/Strikkeapp/Services/TokenService.cs
+++ back/Strikkeapp/Strikkeapp/Services/TokenService.cs
@@ -53,7 +53,7 @@ public class TokenService
     public class TokenResult
     {
         public bool Success { get; set; }
-        public Guid? UserId { get; set; }
+        public Guid UserId { get; set; }
         public string ErrorMessage { get; set; } = string.Empty;
 
         public static TokenResult ForSuccess(Guid userId) => new TokenResult
