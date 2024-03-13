7246e9f2d9bbd3247a8282d8f51d5427dd6d01f3
Sera Madeleine Elstad <sel063@uit.no>
2024-03-13 12:51:09 +0100

    added basick functionality to post and get messages from contact us

--- back/strikkeapp/Strikkeapp/Models/RecipeModels.cs
+++
@@ -1,8 +0,0 @@
-namespace Strikkeapp.Recipes.Models;
-
-public class RecipeInfo
-{
-    public Guid RecipeId { get; set; }
-    public string RecipeName { get; set; } = string.Empty;
-    public string RecipePath { get; set; } = string.Empty;
-}
--- front/src/__tests__/README.md
+++
@@ -1,2 +0,0 @@
-Locate the frontend folder. 
-Run the tests by writing  `npm test` in your terminal.
