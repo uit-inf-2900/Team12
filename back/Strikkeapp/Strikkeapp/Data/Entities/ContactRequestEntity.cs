89d4bcbc424da6c4a3a09ca8dc3854ceeb7af695
SkjalgS <ssl028@uit.no>
2024-03-14 15:14:35 +0100

    :building_construction: Database is now seperated in entities and context

---
+++ back/Strikkeapp/Strikkeapp/Data/Entities/ContactRequestEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Strikkeapp.Data.Entities;
+
+public class ContactRequest
+{
+    // Get -> verdien du får, set-> verdien du setter 
+    // bruker bare get om du vil gjøre den imuteble (ikke mulig å endre) 
+    [Key]
+    public Guid ContactRequestId { get; set; } = Guid.NewGuid();
+
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? Message { get; set; }
+
+    public DateTime TimeCreated { get; set; } = DateTime.Now;
+
+    public bool IsActive { get; set; } = false;
+
+    public bool IsHandled { get; set; } = false;
+}
---
+++ back/Strikkeapp/Strikkeapp/Data/Entities/UserLogInEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Strikkeapp.Data.Entities;
+
+public class UserLogIn
+{
+    [Key]
+    public Guid UserId { get; set; } = Guid.NewGuid();
+
+    [Required]
+    [EmailAddress]
+    public string UserEmail { get; set; } = string.Empty;
+
+    [Required]
+    public string UserPwd { get; set; } = string.Empty;
+
+    [Required]
+    [CustomValidation(typeof(UserLogIn), nameof(ValidateUserStatus))]
+    public string UserStatus { get; set; } = "unverified";
+
+    // Validation for user status
+    private static ValidationResult ValidateUserStatus(string userStatus)
+    {
+        var validStatuses = new List<string> { "unverified", "verified", "banned", "inactive" };
+        if (validStatuses.Contains(userStatus))
+        {
+            return ValidationResult.Success!;
+        }
+        return new ValidationResult("Invalid status");
+    }
+}
