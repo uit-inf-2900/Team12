2c0ae773696bff522568890ed9b3a26cb0269588
Sera Madeleine Elstad <sel063@uit.no>
2024-03-14 12:16:46 +0100

    Add showActive functionality to ViewMessages component

--- front/src/pages/Admin/ViewMessages.js
+++ front/src/pages/Admin/ViewMessages.js
@@ -10,23 +10,32 @@ import "../../GlobalStyles/main.css";
 const ViewMessages = () => {
     const [messages, setMessages] = useState([]);
 
+    // Do the user want to see active or inactive messages?
+    const [showActive, setShowActive] = useState(true); 
+
+
     // Get all the messages from the database
     useEffect(()=> {
-        axios.get('http://localhost:5002/api/Contact?isActive=false')
+        const isActive = showActive ? 'true' : 'false';
+        axios.get(`http://localhost:5002/api/Contact?isActive=${isActive}`)
         .then(response => {
             setMessages(response.data);
         })
         .catch(error => {
             console.log('An error occurred when fetching the messaged from the database',error);
         });
-    }, []);
+    }, [showActive]);
 
     return(
         <div className="page-container">
             <h2> Inncomming messages </h2>
+            <button className='light-button' onClick={() => setShowActive(!showActive)}>
+                Show {showActive ? 'active': 'inactive'} messages
+            </button>
             <ul>
                 {messages.map(message => {
                     return(
+                        // Usees the key to know what message 
                         <li key={message.id}>
                             <h3>From: {message.userName}</h3>
                             <p>Email: {message.userEmail}</p>
