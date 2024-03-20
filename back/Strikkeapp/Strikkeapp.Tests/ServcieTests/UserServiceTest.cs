46c82e6971c02a3abe795271b3ff281e1dccd002
SkjalgS <ssl028@uit.no>
2024-03-20 13:30:35 +0100

    :memo: Added comments

--- back/Strikkeapp/Strikkeapp.Tests/ServcieTests/UserServiceTest.cs
+++ back/Strikkeapp/Strikkeapp.Tests/ServcieTests/UserServiceTest.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Moq;
-
+using Newtonsoft.Json.Bson;
 using strikkeapp.services;
 using Strikkeapp.Data.Context;
 using Strikkeapp.Data.Entities;
@@ -12,8 +12,10 @@ namespace Strikkeapp.Tests
 {
     public class UserServiceTests
     {
+        // Set up db context and service to test
         private readonly StrikkeappDbContext _context;
         private readonly UserService _userService;
+        // Create mock services
         private readonly Mock<IPasswordHasher<object>> _mockPasswordHasher = new Mock<IPasswordHasher<object>>();
         private readonly Mock<ITokenService> _mockTokenService = new Mock<ITokenService>();
 
@@ -35,13 +37,13 @@ namespace Strikkeapp.Tests
         [Fact]
         public void CreateUserTest()
         {
-            // Arrange
+            // Set test variables
             var testEmail = "test@example.com";
             var testPassword = "Test123!";
             var testFullName = "Test Testing";
             var testDob = new DateTime(2024, 1, 1);
-            //var testId = Guid.NewGuid();
 
+            // Mock behaviour
             _mockPasswordHasher.Setup(x => x.HashPassword(It.IsAny<object>(), It.IsAny<string>()))
                     .Returns("hashedPassword");
             _mockTokenService.Setup(x => x.GenerateJwtToken(It.IsAny<string>(), It.IsAny<Guid>()))
@@ -50,23 +52,25 @@ namespace Strikkeapp.Tests
             // Run CreateUser with test variables
             var result = _userService.CreateUser(testEmail, testPassword, testFullName, testDob);
 
-            // Assert
+            // Check expected results
             Assert.True(result.Success);
             _mockPasswordHasher.Verify(x => x.HashPassword(testEmail, testPassword), Times.Once);
             _mockTokenService.Verify(x => x.GenerateJwtToken(testEmail, It.IsAny<Guid>()), Times.Once);
 
-            // Cleanup
+            // Cleanup database
             _context.Database.EnsureDeleted();
         }
 
         [Fact]
         public void LogInUserTest()
         {
+            // Set up test variables
             var testEmail = "test@example.com";
             var testPassword = "Test123!";
             var hashedPassword = "hashedPassword";
             var testId = Guid.NewGuid();
 
+            // Add test user to in memory database
             _context.UserLogIn.Add(new UserLogIn
             {
                 UserEmail = testEmail,
@@ -74,21 +78,25 @@ namespace Strikkeapp.Tests
                 UserId = testId,
             });
 
+            // Save changes to database
             _context.SaveChanges();
 
-
+            // Mock behaviour
             _mockPasswordHasher.Setup(x => x.VerifyHashedPassword(It.IsAny<object>(), hashedPassword, testPassword))
                 .Returns(PasswordVerificationResult.Success);
 
             _mockTokenService.Setup(x => x.GenerateJwtToken(It.IsAny<string>(), It.IsAny<Guid>()))
                      .Returns("fakeToken");
 
+            // Run LogInUser with test variables
             var result = _userService.LogInUser(testEmail, testPassword);
 
+            // Check expected results
             Assert.True(result.Success);
             _mockPasswordHasher.Verify(x => x.VerifyHashedPassword(It.IsAny<object>(), hashedPassword, testPassword), Times.Once);
             _mockTokenService.Verify(x => x.GenerateJwtToken(testEmail, It.IsAny<Guid>()), Times.Once);
 
+            // Cleanup database
             _context.Database.EnsureDeleted();
         }
     }
