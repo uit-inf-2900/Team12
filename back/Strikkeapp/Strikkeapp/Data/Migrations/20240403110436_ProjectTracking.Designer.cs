e4f7ce2cdf6594e779a8e1f1464516b98252fa62
SkjalgS <ssl028@uit.no>
2024-04-03 13:12:16 +0200

    :card_file_box: Addedmigration for PT

--- back/Strikkeapp/Strikkeapp/Data/Migrations/20240403110436_ProjectTracking.Designer.cs
+++ back/Strikkeapp/Strikkeapp/Data/Migrations/20240403110436_ProjectTracking.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Strikkeapp.Data.Context;
 
@@ -10,9 +11,11 @@ using Strikkeapp.Data.Context;
 namespace Strikkeapp.Data.Migrations
 {
     [DbContext(typeof(StrikkeappDbContext))]
-    partial class StrikkeappDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240403110436_ProjectTracking")]
+    partial class ProjectTracking
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.3");
