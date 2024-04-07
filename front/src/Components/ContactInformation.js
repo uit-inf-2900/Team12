4af9bb233fd78762fe64e03116c723edeca6fbce
SeraMadeleine <sel063@uit.no>
2024-04-07 13:33:47 +0200

    added space between icon and text for contact us

--- front/src/Components/ContactInformation.js
+++ front/src/Components/ContactInformation.js
@@ -6,9 +6,18 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 const ContactInformation = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
-            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}> <EmailIcon /> <a href="mailto:contact@knithub.com">contact@knithub.com</a></p>
-            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}> <PhoneInTalkIcon /> <a href="tel:+4712345678">12345678</a></p>
-            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}> <LocationOnIcon /> <a href="https://maps.google.com?q=Hansine+Hansens+veg+56,+9019+Tromsø">Hansine Hansens veg 56, 9019 Tromsø</a> </p>
+            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+                <EmailIcon sx={{ marginRight: "10px" }} /> 
+                <a href="mailto:contact@knithub.com">contact@knithub.com</a>
+            </p>
+            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+                <PhoneInTalkIcon sx={{ marginRight: "10px" }} /> 
+                <a href="tel:+4712345678">12345678</a>
+            </p>
+            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+                <LocationOnIcon sx={{ marginRight: "10px" }} /> 
+                <a href="https://maps.google.com?q=Hansine+Hansens+veg+56,+9019+Tromsø">Hansine Hansens veg 56, 9019 Tromsø</a>
+            </p>
         </div>
     );
 };
