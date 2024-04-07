67f1f8ea9d72ef3f9b7845faf074aa8b5b1269f7
SeraMadeleine <sel063@uit.no>
2024-04-07 15:47:06 +0200

    impoved the footer

--- front/src/Components/Footter.js
+++ front/src/Components/Footter.js
@@ -1,90 +1,127 @@
 import React, { useState } from 'react';
-import { Box, Grid, Typography, Link, IconButton, Paper } from '@mui/material';
-import Input from '@mui/material/Input';
-import { NavLink } from "react-router-dom";
+import { Link, Box, Grid, Typography, IconButton, Paper, Button } from '@mui/material';
 import Theme from './Theme';
 import { ThemeProvider } from '@mui/material/styles';
+import { useForm } from 'react-hook-form';
+
 
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import PinterestIcon from '@mui/icons-material/Pinterest';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import ContactInformation from './ContactInformation'; 
 import InputField from './InputField';
-import Button from './Button'
+import CustomButton from './Button'
+import SendIcon from '@mui/icons-material/Send';
+
+// TODO: Get error if no email is entered
+// TODO: Get error if email is not valid
+// TODO: Get info allert if email is already subscribed
+// TODO: Get success alert if the subscribtion now is successful
+// TODO: Get error alert if the subscribtion is not successful
+
 
 // Pages the footer should link to
 const FooterRouting = () => {
     return (
-        <Box>
+        <div>
             <Typography variant="h3" >
                 Useful Links
             </Typography>
-            <Link href="/home"> Home </Link><br />
-            <Link href="/about"> About </Link><br />
-            <Link href="/contactus" > Contact us </Link><br />
-        </Box>
+            <Link color='#000000' href="/"> Home </Link><br />
+            <Link color='#000000' href="/about"> About </Link><br />
+            <Link color='#000000' href="/contactus" > Contact us </Link><br />
+        </div>
     ); 
 };
 
 const SomeFooter = () => {
     return (
         <Box>
-            <IconButton >
-                <FacebookIcon />
-            </IconButton>
-            <IconButton >
-                <InstagramIcon />
-            </IconButton>
-            <IconButton >
-                <PinterestIcon />
-            </IconButton>
-            <IconButton >
-                <TwitterIcon />
-            </IconButton>
+            <a href="https://www.facebook.com/" >
+                <IconButton>
+                        <FacebookIcon />
+                </IconButton>
+            </a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                <IconButton>
+                    <InstagramIcon />
+                </IconButton>
+            </a>
+            <a href="https://www.pinterest.com/" target="_blank" rel="noopener noreferrer">
+                <IconButton>
+                    <PinterestIcon />
+                </IconButton>
+            </a>
+            <a href="https://github.com/uit-inf-2900/Team12" target="_blank" rel="noopener noreferrer">
+                <IconButton>
+                    <GitHubIcon />
+                </IconButton>
+            </a>
         </Box>
     ); 
 };
 
 const Footer = () => {
     const [email, setEmail] = useState('');
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
 
     const handleSubscribe = () => {
-        // Implement your subscription logic here
-        alert(`Subscribe to the newsletter with: ${email}`);
+        // TODO: implement the subscribe functionality
+        if(email) {
+            alert(`Subscribe to the newsletter with: ${email}`);
+            // Reset email state
+            setEmail('');
+        } else {
+            // Handle empty input or add validation
+            alert('Please enter an email address.');
+        }
     };
 
+    
+
     return (
+        // Use paper for the color (can be changed in Theme)
+        // Set the with to 100% and add some padding 
         <Paper sx={{ width: '100%', py: 4 }}> 
             <Grid container spacing={3} justifyContent="center" alignItems="start">
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={12} sm={4}>
                     <Typography variant="h3">
                         Knithub
                     </Typography>
-                    <Box component="form" onSubmit={handleSubscribe} noValidate sx={{ mt: 1 }}>
+                    <Box component="form"
+                        onSubmit={handleSubmit(handleSubscribe)}
+                        noValidate
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'flex',
+                            mt: 1,
+                        }}>
                         <InputField
                             fullWidth
                             label="Subscribe to our newsletter"
                             variant="outlined"
-                            type="email"
+                            type="send"
                             value={email}
                             onInput={e => setEmail(e.target.value)}
-                            sx={{ mb: 2 }}
+                            error={errors.email}
+                            helperTect={errors.email?.message}
+                            onSubmit={handleSubscribe}
                         />
-                        <Button iconName='send'> </Button>
                     </Box>
                     <SomeFooter />
                 </Grid>
                 <Grid item xs={12} sm={3}>
                     <FooterRouting />
                 </Grid>
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={12} sm={4}>
                     <Typography variant="h3" gutterBottom>
                         Contact Us
                     </Typography>
                     <ContactInformation />
                 </Grid>
-                <Grid item xs={12} sx={{ mt: 4, textAlign: 'center' }}>
+                <Grid item xs={9} sx={{ mt: 4, textAlign: 'center' }}>
                     <Typography variant="body2">
                         © {new Date().getFullYear()} Knithub. All rights reserved.
                     </Typography>
--- front/src/Components/Theme.js
+++ front/src/Components/Theme.js
@@ -66,6 +66,16 @@ const Theme = (mode) => {
                             backgroundColor: isDarkMode ? 'var(--button-hover-dark)' : 'var(--button-hover-light)',
                             opacity: 0.8,
                         },
+                    outlined: {
+                        color: '#fffff',
+                        justifyContent: 'center', 
+                        borderColor: '#fffff',
+                        backgroundColor: 'transparent', 
+                        '&:hover': {
+                            borderColor: '#fffff', // Endre border farge på hover
+                            backgroundColor: 'transparent', // Oppretthold transparent bakgrunn på hover
+                        },
+                    }, 
                     },
                 },
             },
