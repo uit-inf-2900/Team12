5da68a8c5856dbe429da69a25e6a6fd31fb7e97e
SkjalgS <ssl028@uit.no>
2024-04-08 12:15:52 +0200

    :sparkles: Added controller for service

---
+++ back/Strikkeapp/Strikkeapp/Controllers/InventoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Strikkeapp.Services;
+
+namespace Strikkeapp.Controllers;
+
+[Route("api/inventory")]
+[ApiController]
+public class InventoryController : ControllerBase
+{
+    private readonly IInventoryService _inventoryService;
+
+    public InventoryController(IInventoryService inventoryService)
+    {
+        _inventoryService = inventoryService;
+    }
+
+    [HttpGet]
+    [Route("/get_inventory")]
+    public IActionResult GetInventory([FromQuery] string userToken)
+    {
+        var res = _inventoryService.GetInventory(userToken);
+        if(!res.Success) 
+        {
+            if(res.ErrorMessage == "Unauthorized")
+            {
+                return Unauthorized("Invalid token");
+            }
+
+            return (StatusCode(500, "Could not fetch inventory"));
+        }
+
+        return Ok(new
+        {
+            yarnInventory = res.YarnInventories,
+            needleInventory = res.NeedleInventories
+        });
+    }
+}
