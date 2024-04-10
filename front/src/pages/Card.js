965c5381ff954539d4a3bd5153eee110d6cfff38
SeraMadeleine <sel063@uit.no>
2024-04-10 12:07:12 +0200

    fixed indent for card

--- front/src/pages/Card.js
+++ front/src/pages/Card.js
@@ -5,22 +5,22 @@ import "../GlobalStyles/main.css";
 
 const Card = ({ title, description }) => {
     
-  return (
+    return (
     <div className="card">
       <img src={exampleImage} alt={title} className="card-image" /> {/* Use imported image */}
-      <div className="card-overlay">
+        <div className="card-overlay">
             <div className="card-header">
-              <div className="card-header-text">
-                <h3 className="card-title">{title}</h3>
-              </div>
+                <div className="card-header-text">
+                    <h3 className="card-title">{title}</h3>
+                </div>
             </div>
             <p className="card-description">Needle size</p>
             <p className='card-description'>Knitting Gauge</p>
             <p className='card-description'>Notes</p>
-          </div>
+        </div>
         
     </div>
-  );
+    );
 };
 
 export default Card;
