bbdad07712f3ad44a19641c11f49874125d712e5
SeraMadeleine <sel063@uit.no>
2024-04-15 08:26:19 +0200

    removed table from home page

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "../GlobalStyles/main.css";
-import KnittingTermsTable from "./KnittingTermsTable"; 
 
 
 export const Home = () => {
   return (
     <div className="page-container">
       <h1>Home </h1>      
-      {/* Rest of the content */}
-      <KnittingTermsTable/> 
     </div>
   );
 };
