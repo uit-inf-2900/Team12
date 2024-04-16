7f25aeee98548097ace0f2ce8f73c2e489347515
SeraMadeleine <sel063@uit.no>
2024-04-16 09:29:37 +0200

    InstagramFeed: Add loading state and error handling

--- front/src/pages/KnitHubResources/InstagramFeed.js
+++ front/src/pages/KnitHubResources/InstagramFeed.js
@@ -2,18 +2,47 @@ import React, { useEffect, useState } from 'react';
 
 const InstagramFeed = ({ accessToken }) => {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+
+    // Fetch Instagram posts
     const fetchInstagramPosts = async () => {
-      const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${accessToken}`);
-      const data = await response.json();
-      // Shuffel the posts and only show 16 of them 
-      const shuffledPosts = data.data.sort(() => 0.5 - Math.random()).slice(0, 16);
-      setPosts(shuffledPosts);
+      try {
+        const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink,thumbnail_url,timestamp&access_token=${accessToken}`);
+        
+        // Check if the response is ok
+        if (!response.ok) {
+          throw new Error('Failed to fetch Instagram posts');
+        }
+        // Get the data, and shuffel the posts and only show 16 of them 
+        const data = await response.json();
+        const shuffledPosts = data.data.sort(() => 0.5 - Math.random()).slice(0, 16);
+        setPosts(shuffledPosts);
+      }
+      catch (error) {
+        setError(error.message);
+      }
+      finally {
+        setLoading(false);
+      }
     };
 
     fetchInstagramPosts();
   }, [accessToken]);
+
+
+  // If the posts are loading, show a loading message
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // If there is an error, show the error message
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
 
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '10px' }}>
