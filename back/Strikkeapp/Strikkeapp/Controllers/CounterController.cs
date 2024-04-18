fd4acb49f4e49e9dcc44fa3040d42b3572e0c41c
SkjalgS <ssl028@uit.no>
2024-04-18 12:58:09 +0200

    Can now create and update counter

---
+++ back/Strikkeapp/Strikkeapp/Controllers/CounterController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Strikkeapp.Models;
+using Strikkeapp.Services;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Strikkeapp.Controllers;
+
+[Route("api/counter")]
+[ApiController]
+public class CounterController : ControllerBase
+{
+    private readonly ICounterService _counterService;
+
+    public CounterController(ICounterService counterService)
+    {
+        _counterService = counterService;
+    }
+
+    [HttpPost]
+    [Route("createcounter")]
+    public IActionResult CreateCounter([FromBody] CreateCounterRequest request)
+    {
+        if (!request.IsOK())
+        {
+            return BadRequest();
+        }
+
+        var res = _counterService.CreateCounter(request.userToken, request.name);
+
+        if (!res.Success)
+        {
+            if (res.ErrorMessage == "Unauthorized")
+            {
+                return Unauthorized();
+            }
+
+            return StatusCode(500, res.ErrorMessage);
+        }
+
+        return Ok(res.CounterId);
+    }
+
+    [HttpPatch]
+    [Route("updatecounter")]
+    public IActionResult UpdateCounter([FromBody] UpdateCounterRequest request)
+    {
+        if (!request.IsOk())
+        {
+            return BadRequest();
+        }
+
+        var res = _counterService.UpdateCounter(request.userToken, request.counterId, request.newNum);
+
+        if (!res.Success)
+        {
+            if (res.ErrorMessage == "Unauthorized")
+            {
+                return Unauthorized();
+            }
+            if (res.ErrorMessage == "Not found")
+            {
+                return NotFound("Could not find counter");
+            }
+
+            return StatusCode(500, res.ErrorMessage);
+        }
+
+        return Ok();
+    }
+
+
+
+}
