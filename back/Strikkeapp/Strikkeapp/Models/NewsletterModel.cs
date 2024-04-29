82054be7c525c63f7f11af03a6629a98e4b04da2
SkjalgS <ssl028@uit.no>
2024-04-29 10:29:52 +0200

    :building_construction: Model for newsletter serivce

---
+++ back/Strikkeapp/Strikkeapp/Models/NewsletterModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Strikkeapp.Models;
+
+public class NewsletterResult
+{
+    public bool Success { get; set; }
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public static NewsletterResult ForSuccess() => new NewsletterResult
+    { Success = true };
+
+    public static NewsletterResult ForFailure(string message) => new NewsletterResult
+    {
+        Success = false,
+        ErrorMessage = message
+    };
+}
+
+public class NewsletterSubscribersResult
+{
+    public bool Success { get; set; }
+    public string ErrorMessage { get; set; } = string.Empty;
+    public List<string> Subscribers { get; set; } = new List<string>();
+
+    public static NewsletterSubscribersResult ForSuccess(List<string> subscribers) => new NewsletterSubscribersResult
+    {
+        Success = true,
+        Subscribers = subscribers
+    };
+
+    public static NewsletterSubscribersResult ForFailure(string message) => new NewsletterSubscribersResult
+    {
+        Success = false,
+        ErrorMessage = message
+    };
+}
