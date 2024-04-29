33db29c870dadeb7aec20890140d40b85f7c2316
SkjalgS <ssl028@uit.no>
2024-04-29 10:29:30 +0200

    :sparkles: Added newsletter controller and service

---
+++ back/Strikkeapp/Strikkeapp/Controllers/NewsletterController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Strikkeapp.Services;
+
+namespace Strikkeapp.Controllers;
+
+[Route("api/newsletter")]
+[ApiController]
+public class NewsletterController : ControllerBase
+{
+    private readonly INewsletterService _newsletterService;
+
+    public NewsletterController(INewsletterService newsletterService)
+    {
+        _newsletterService = newsletterService;
+    }
+
+    [HttpPost]
+    [Route("addsubscriber")]
+    public IActionResult AddSubscriber([FromQuery] string subEmail)
+    {
+        var res = _newsletterService.SubscribeToNewsletter(subEmail);
+
+        if(!res.Success)
+        {
+            if(res.ErrorMessage == "Duplicate")
+            {
+                return Conflict("Email is already in subscribed");
+            }
+
+            return StatusCode(500, res.ErrorMessage);
+        }
+
+        return Ok("User subscribed to newsletter");
+    }
+
+    [HttpDelete]
+    [Route("removesubscriber")]
+    public IActionResult RemoveSubscriber([FromQuery] string subEmail)
+    {
+        var res = _newsletterService.RemoveSubscriber(subEmail);
+
+        if(!res.Success) 
+        {
+            if(res.ErrorMessage == "Not found")
+            {
+                return NotFound("Email not found in subsciption list");
+            }
+
+            return StatusCode(500, res.ErrorMessage);
+        }
+
+        return Ok("User has unsubscribed");
+    }
+
+    [HttpGet]
+    [Route("getsunscribers")]
+    public IActionResult GetSubscibers([FromQuery] string userToken)
+    {
+        var res = _newsletterService.GetUsers(userToken);
+
+        if(!res.Success)
+        {
+            if(res.ErrorMessage == "Unauthorized")
+            {
+                return Unauthorized("User not allowed to see subscribers");
+            }
+
+            return StatusCode(500, res.ErrorMessage);
+        }
+
+        return Ok(res.Subscribers);
+    }
+
+}
---
+++ back/Strikkeapp/Strikkeapp/Services/NewsletterService.cs
@@ -0,0 +1,127 @@
+using Strikkeapp.Data.Context;
+using Strikkeapp.Data.Entities;
+using Strikkeapp.Models;
+
+namespace Strikkeapp.Services; 
+
+public interface INewsletterService
+{
+    public NewsletterResult SubscribeToNewsletter(string subscriberEmail);
+    public NewsletterResult RemoveSubscriber(string subscriberEmail); 
+    public NewsletterSubscribersResult GetUsers(string token);
+}
+
+public class NewsletterService : INewsletterService
+{
+    private readonly StrikkeappDbContext _context;
+    private readonly ITokenService _tokenService;
+
+    public NewsletterService(StrikkeappDbContext context, ITokenService tokenService)
+    {
+        _context = context;
+        _tokenService = tokenService;
+    }
+
+    public NewsletterResult SubscribeToNewsletter(string subscriberEmail)
+    {
+        // Check that email is not already in db
+        if(_context.Newsletter.Any(se => se.email == subscriberEmail))
+        {
+            return NewsletterResult.ForFailure("Duplicate");
+        }
+
+        // Add email to db
+        using(var transaction = _context.Database.BeginTransaction()) 
+        {
+            try 
+            {
+                var newEntry = new Newsletter
+                {
+                    email = subscriberEmail
+                };
+
+                _context.Newsletter.Add(newEntry);
+                _context.SaveChanges();
+
+                transaction.Commit();
+                return NewsletterResult.ForSuccess();
+            }
+            catch (Exception ex) 
+            {
+                // Rollback and return errormessage if exception should occur
+                transaction.Rollback();
+                return NewsletterResult.ForFailure(ex.Message);
+            }
+        }
+    }
+
+    public NewsletterResult RemoveSubscriber(string subscriberEmail) 
+    {
+        using(var transaction = _context.Database.BeginTransaction())
+        {
+            try
+            {
+                // Get entry and check it exsits in db
+                var entryForDeletion = _context.Newsletter
+                    .FirstOrDefault(se => se.email == subscriberEmail);
+
+                if(entryForDeletion == null)
+                {
+                    return NewsletterResult.ForFailure("Not found");
+                }
+
+                // Remove entry and save changes
+                _context.Newsletter.Remove(entryForDeletion);
+                _context.SaveChanges();
+
+                transaction.Commit();
+                return NewsletterResult.ForSuccess();
+            }
+            catch(Exception ex) 
+            { 
+                // Rollback and return errormessage if exception should occur
+                transaction.Rollback();
+                return NewsletterResult.ForFailure(ex.Message);
+            }
+        }
+    }
+
+    public NewsletterSubscribersResult GetUsers(string token)
+    {
+        // Get user token, and handle error if it cannot be retrieved
+        var tokenResult = _tokenService.ExtractUserID(token);
+        if(tokenResult == null) 
+        {
+            return NewsletterSubscribersResult.ForFailure("Unauthorized");
+        }
+
+        // Extract the userid
+        var userId = tokenResult.UserId;
+
+        try
+        {
+            // Check that the requesting user is an admin
+            var user = _context.UserDetails
+                .FirstOrDefault(ud => ud.UserId == userId);
+
+            if(user == null || user.IsAdmin != true)
+            {
+                return NewsletterSubscribersResult.ForFailure("Unauthorized");
+            }
+
+            // Get list of subscribers and return it
+            var subscribers = _context.Newsletter
+                .Select(e => e.email)
+                .ToList();
+
+
+            return NewsletterSubscribersResult.ForSuccess(subscribers);
+        }
+
+        catch(Exception ex)
+        {
+            // Return error message if exception should occur
+            return NewsletterSubscribersResult.ForFailure(ex.Message);
+        }
+    }
+}
