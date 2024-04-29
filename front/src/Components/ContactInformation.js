a9aee23f82dfa4c0470780854dd564a45898492b
SeraMadeleine <sel063@uit.no>
2024-04-29 16:28:02 +0200

    added test for contact information

--- front/src/Components/ContactInformation.js
+++ front/src/Components/ContactInformation.js
@@ -6,15 +6,15 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 const ContactInformation = () => {
     return (
         <div style={{ display: "flex", flexDirection: "column" }}>
-            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }} data-testid="email">
                 <EmailIcon sx={{ marginRight: "10px" }} /> 
                 <a href="mailto:contact@knithub.com">contact@knithub.com</a>
             </p>
-            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }} data-testid="phone">
                 <PhoneInTalkIcon sx={{ marginRight: "10px" }} /> 
                 <a href="tel:+4712345678">12345678</a>
             </p>
-            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
+            <p style={{ display: "flex", alignItems: "center", marginBottom: "10px" }} data-testid="address">
                 <LocationOnIcon sx={{ marginRight: "10px" }} /> 
                 <a href="https://maps.google.com?q=Hansine+Hansens+veg+56,+9019+Tromsø">Hansine Hansens veg 56, 9019 Tromsø</a>
             </p>
