f4fd9728618de364dd17d07339673bed4147005f
SeraMadeleine <sel063@uit.no>
2024-04-30 15:48:25 +0200

    added comments to imput field

--- front/src/Components/InputField.js
+++ front/src/Components/InputField.js
@@ -4,13 +4,24 @@ import SendIcon from '@mui/icons-material/Send';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
+
+
+/**
+ * InputField component renders various types of input fields, including text, password, select, and send.
+ */
 const InputField = ({
     label, register, errors, type, readOnly, onSubmit, value, onChange, options = [], ...inputProps
 }) => {
+    // Components for the password field to show/hide the password 
+    // and the send button to send the form
     const isPassword = type === "password";
     const [showPassword, setShowPassword] = React.useState(false);
     const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
+
+    /**
+     * Handle click on send button
+     * */ 
     const handleSendClick = (event) => {
         if (errors) {
             return;
@@ -26,6 +37,8 @@ const InputField = ({
             color='secondary'
             {...register}
             {...inputProps}
+
+            // Toggle password visibility if it's a password field and visibility icon is clicked
             type={isPassword && showPassword ? "text" : type}
             label={label}
             error={!!errors}
@@ -35,16 +48,21 @@ const InputField = ({
             fullWidth
             onChange={onChange}
             value={value}
+
+            // Render as select input if type is select
             select={type === "select"}
             InputProps={{
                 readOnly: readOnly,
+                // Add end adornment (icon at the end of input field)
                 endAdornment: (
                 <InputAdornment position="end">
                     {isPassword ? (
+                         // Render visibility icon for password field
                         <IconButton aria-label="Toggle password visibility" onClick={togglePasswordVisibility}>
                             {showPassword ? <VisibilityOff /> : <Visibility />}
                         </IconButton>
                     ) : type === "send" ? (
+                        // Render send button for send type input
                         <IconButton onClick={handleSendClick}>
                             <SendIcon />
                         </IconButton>
@@ -54,6 +72,8 @@ const InputField = ({
                 ...inputProps.InputProps,
             }}
             >
+
+            {/* Render menu items for select type input */}
             {type === "select" && options.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
                 {option.label}
