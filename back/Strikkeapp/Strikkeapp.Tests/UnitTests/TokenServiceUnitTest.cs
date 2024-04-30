9a76744c1973620bc8182468c420bcee96f2a4e7
SkjalgS <ssl028@uit.no>
2024-04-30 14:23:28 +0200

    :white_check_mark: Invcalid token fails

--- back/Strikkeapp/Strikkeapp.Tests/UnitTests/TokenServiceUnitTest.cs
+++ back/Strikkeapp/Strikkeapp.Tests/UnitTests/TokenServiceUnitTest.cs
@@ -11,6 +11,7 @@ public class TokenServiceTests
 
     public TokenServiceTests()
     {
+        // Set up configuration
         var config = new Mock<IConfiguration>();
         config.SetupGet(x => x["Jwt:Key"]).Returns("0123456789ABCDEF0123456789ABCDEF");
         config.SetupGet(x => x["Jwt:Issuer"]).Returns("YourIssuer");
@@ -22,11 +23,13 @@ public class TokenServiceTests
     [Fact]
     public void Valid_Token_Returned()
     {
+        // Set up test data
         string testEmail = "test@example.com";
         Guid testGuid = Guid.NewGuid();
         bool testAdmin = false;
         string testStatus = "unverified";
 
+        // Generate token and ensure it is not null or empty
         string token = _tokenService.GenerateJwtToken(testEmail, testGuid, testAdmin, testStatus);
 
         Assert.NotNull(token);
@@ -36,15 +39,19 @@ public class TokenServiceTests
     [Fact]
     public void UserID_Success_ValidToken()
     {
+        // Set up test data
         string testEmail = "test@example.com";
         Guid testGuid = Guid.NewGuid();
         bool testAdmin = false;
         string testStatus = "unverified";
 
+        // Generate token
         string token = _tokenService.GenerateJwtToken(testEmail, testGuid, testAdmin, testStatus);
 
+        // Extract user ID from token
         var res = _tokenService.ExtractUserID(token);
 
+        // Assert if fail or mismatch
         Assert.True(res.Success);
         Assert.Equal(res.UserId, testGuid);
     }
@@ -62,12 +69,26 @@ public class TokenServiceTests
     [Fact]
     public void InvalidKey_Fails()
     {
+        // Setup configuration with empty key
         var config = new Mock<IConfiguration>();
         config.SetupGet(x => x["Jwt:Key"]).Returns("");
         config.SetupGet(x => x["Jwt:Issuer"]).Returns("YourIssuer");
         config.SetupGet(x => x["Jwt:Audience"]).Returns("YourAudience");
 
-        TokenService tokenService = null;
+        // Run serivce and expect exception
+        TokenService tokenService = null!;
         Assert.Throws<InvalidOperationException>(() => tokenService = new TokenService(config.Object));
+    }
+
+    [Fact]
+    public void InvalidToken_Fails()
+    {
+        // Set up and run serivce
+        var invalidToken = "invalidToken";
+        var result = _tokenService.ExtractUserID(invalidToken);
+
+        // Assert if sucess and ensure correct error
+        Assert.False(result.Success);
+        Assert.True(result.ErrorMessage.Contains("Token not found") || result.ErrorMessage.Contains("Error while processing token"));
     }
 }
