4be349a6283b5601ff2e99787edef4ae5119d0a1
SeraMadeleine <sel063@uit.no>
2024-05-02 12:51:54 +0200

    added comments and dockstrings

--- front/src/Components/Button.js
+++ front/src/Components/Button.js
@@ -12,8 +12,12 @@ import AddIcon from '@mui/icons-material/Add';
 
 import Theme from './Theme';
 
-// Select a fitting icon based on the iconName prop
-const IconSelector = ({ iconName }) => {
+
+/**
+ * Component that selects an icon based on the provided `iconName` prop.
+ * @param {string} iconName - Name of the icon to select.
+ * @returns {JSX.Element} - The selected icon component.
+ */const IconSelector = ({ iconName }) => {
     const icons = {
         send: <SendIcon />,
         delete: <DeleteIcon />,
@@ -29,7 +33,22 @@ const IconSelector = ({ iconName }) => {
 };
 
 
+
+/**
+ * Custom button component.
+ * @param {object} props - Component props.
+ * @param {React.ReactNode} props.children - Button content.
+ * @param {string} props.choosenvar - Custom variable.
+ * @param {string} props.iconName - Name of the icon to display on the button.
+ * @param {string} props.themeMode - Theme mode for the button.
+ * @param {boolean} props.submit - Indicates if the button is a submit button.
+ * @param {boolean} props.fullWidth - Indicates if the button should take full width.
+ * @param {function} props.onClick - Function to handle click events.
+ * @returns {JSX.Element} - The custom button component.
+ */
 const CustomButton = ({ children, choosenvar, iconName, themeMode, submit, fullWidth, onClick, ...props  }) => {
+    // Choose a theme based on the themeMode prop, or default to light theme 
+    // the button will be white or grey depending on the themeMode
     const theme = Theme(themeMode || 'light'); 
 
     const buttonStyle = {
@@ -53,6 +72,12 @@ const CustomButton = ({ children, choosenvar, iconName, themeMode, submit, fullW
 };
 
 
+/**
+ * Add button component.
+ * @param {object} props - Component props.
+ * @param {function} props.onClick - Function to handle click events.
+ * @returns {JSX.Element} - The add button component.
+ */
 const AddButton = ({ onClick }) => {
     const [isHovered, setIsHovered] = useState(false);
 
@@ -64,6 +89,7 @@ const AddButton = ({ onClick }) => {
     };
 
 
+    /** Hover styles for the button */
     const hoverStyle = {
         backgroundColor: '#d06514', 
         transform: 'scale(1.1)', 
