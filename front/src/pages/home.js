c3acd7fd4f04af775faf960cbf6c3701af434c0f
SeraMadeleine <sel063@uit.no>
2024-05-02 15:45:00 +0200

    removed unused code

--- front/src/pages/home.js
+++ front/src/pages/home.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import "../GlobalStyles/main.css";
-import './home.css';
 import axios from 'axios';
 import StatisticBox from './StatisticBox'; // Sjekk at stien stemmer
 import { useNavigate } from 'react-router-dom';
