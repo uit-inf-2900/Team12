e126a67aeff66cf3ffe0fe0a5d4393530fce737f
Anna Eline Wefring De Vito <avi064@uit.no>
2024-05-03 15:05:25 +0200

    fixed bug in logout

--- front/src/pages/SignUp_LogIn/ConfirmationLogout.js
+++ front/src/pages/SignUp_LogIn/ConfirmationLogout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CustomButton from "../../Components/Button";
+import { CustomButton } from '../../Components/Button';
 import '../../GlobalStyles/BoxAndContainers.css';
 import { Modal, Box } from '@mui/material';
 
