0e8d3b9cd13659fce48c83f2d6f9de70e76865a8
SkjalgS <ssl028@uit.no>
2024-05-08 11:52:08 +0200

    :white_check_mark: Update tests

--- back/Strikkeapp/Strikkeapp.Tests/IntegrationTest/Controllers/CounterControllerTests.cs
+++ back/Strikkeapp/Strikkeapp.Tests/IntegrationTest/Controllers/CounterControllerTests.cs
@@ -26,7 +26,7 @@ public class CounterControllerTests
 
     // Test variables
     private Guid testUserId = Guid.NewGuid();
-    private Guid counterId = Guid.NewGuid();
+    private Guid testCounterId = Guid.NewGuid();
 
     public CounterControllerTests()
     {
@@ -66,7 +66,7 @@ public class CounterControllerTests
         // Add counter to db
         _context.CounterInventory.Add(new Counter
         {
-            CounterId = counterId,
+            CounterId = testCounterId,
             UserId = testUserId,
             RoundNumber = 5,
             Name = "test counter"
@@ -143,4 +143,71 @@ public class CounterControllerTests
         var response = _controller.GetCounter("fakeToken");
         Assert.IsType<UnauthorizedResult>(response);
     }
+
+    [Fact]
+    public void UpdateCounter_Ok()
+    {
+        // Call controller with valid request and verify response
+        var request = new UpdateCounterRequest
+        {
+            userToken = "userToken",
+            counterId = testCounterId,
+            newName = "updated counter"
+        };
+
+        var response = _controller.UpdateCounter(request);
+        Assert.IsType<OkResult>(response);
+    }
+
+    [Fact]
+    public void BadRequestUpdate_Fails()
+    {
+        // Empty request
+        var request = new UpdateCounterRequest
+        {
+            userToken = "",
+            counterId = Guid.Empty,
+            newName = ""
+        };
+
+        // Call controller with empty request and verify response
+        var response = _controller.UpdateCounter(request);
+        Assert.IsType<BadRequestResult>(response);
+    }
+
+    [Fact]
+    public void FakeTokenUpdate_Fails()
+    {
+        // Mock service to return unauthorized
+        _mockTokenService.Setup(x => x.ExtractUserID("fakeToken"))
+            .Returns(TokenResult.ForFailure("Unauthorized"));
+
+        // Call controller with fake token and verify response
+        var request = new UpdateCounterRequest
+        {
+            userToken = "fakeToken",
+            counterId = testCounterId,
+            newName = "updated counter"
+        };
+
+        var response = _controller.UpdateCounter(request);
+        Assert.IsType<UnauthorizedResult>(response);
+    }
+
+    [Fact]
+    public void NonCounterUpdate_Fails()
+    {
+        // Call controller with non counter id
+        var request = new UpdateCounterRequest
+        {
+            userToken = "userToken",
+            counterId = Guid.NewGuid(),
+            newName = "updated counter"
+        };
+
+        var response = _controller.UpdateCounter(request);
+        Assert.IsType<NotFoundObjectResult>(response);
+    }
+
+
 }
