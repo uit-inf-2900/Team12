3bbb3c039ed6cb3a466e978d174ff91110e5683f
SkjalgS <ssl028@uit.no>
2024-05-11 18:13:59 +0200

    :white_check_mark: Fake token fails

---
+++ back/Strikkeapp/Strikkeapp.Tests/IntegrationTest/Controllers/RecipeControllerTests.cs
@@ -0,0 +1,216 @@
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+
+using Strikkeapp.Controllers;
+using Strikkeapp.Services;
+using Strikkeapp.Data.Context;
+using Strikkeapp.Data.Entities;
+using Strikkeapp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace Strikkeapp.Tests.Controllers;
+
+public class RecipeControllerTests : IDisposable
+{
+    // Initialize db, controller and services
+    private readonly RecipeController _controller;
+    private readonly RecipeService _recipeService;
+    private readonly StrikkeappDbContext _context;
+    private readonly IConfiguration _mockConfiguration;
+
+    // Set up mocks
+    private readonly Mock<IPasswordHasher<object>> _mockPasswordHasher = new Mock<IPasswordHasher<object>>();
+    private readonly Mock<ITokenService> _mockTokenService = new Mock<ITokenService>();
+    private readonly string _mockStoragePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+    // Test data
+    private Guid testUserId = Guid.NewGuid();
+    private Guid testRecipeId = Guid.NewGuid();
+    private Guid testAdminId = Guid.NewGuid();
+
+    public RecipeControllerTests()
+    {
+        // Set up mock services
+        _mockPasswordHasher.Setup(x => x.HashPassword(It.IsAny<object>(), It.IsAny<string>()))
+            .Returns("hashedPassword");
+
+        _mockTokenService.Setup(x => x.ExtractUserID("adminToken"))
+            .Returns(TokenResult.ForSuccess(testAdminId));
+        _mockTokenService.Setup(x => x.ExtractUserID("userToken"))
+            .Returns(TokenResult.ForSuccess(testUserId));
+        _mockTokenService.Setup(x => x.ExtractUserID("fakeToken"))
+            .Returns(TokenResult.ForFailure("Unauthorized"));
+
+        // Set up in-memory database
+        var dbName = Guid.NewGuid().ToString();
+        var options = new DbContextOptionsBuilder<StrikkeappDbContext>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .ConfigureWarnings(warnings => warnings.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        // Set up the mock configuration for storage path
+        var inMemorySettings = new Dictionary<string, string> 
+        {
+            {"ConnectionStrings:RecipesStorage", _mockStoragePath}
+        };
+        _mockConfiguration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings!)
+            .Build();
+
+        // Set up context, service and controller
+        _context = new StrikkeappDbContext(options, _mockPasswordHasher.Object);
+        _recipeService = new RecipeService(_mockConfiguration, _mockTokenService.Object, _context);
+        _controller = new RecipeController(_recipeService);
+
+        // Seed database
+        SeedDatabase();
+    }
+
+    private void SeedDatabase()
+    {
+        // Ensure the storage path exists. If not, create it
+        if (!Directory.Exists(_mockStoragePath))
+        {
+            Directory.CreateDirectory(_mockStoragePath);
+        }
+
+        // Create a test recipe
+        var recipeFilePath = Path.Combine(_mockStoragePath, $"{testRecipeId}.pdf");
+        File.WriteAllBytes(recipeFilePath, new byte[] { 1, 2, 3, 4, 5 });
+
+        _context.KnittingRecipes.Add(new KnittingRecipes
+        {
+            KnittingRecipeId = testRecipeId,
+            UserId = testUserId,
+            RecipeName = "Test Recipe",
+            NeedleSize = 5,
+            KnittingGauge = "10x10",
+            Notes = "Test notes",
+            RecipePath = recipeFilePath
+        });
+
+        // Add test user to database
+        _context.UserLogIn.Add(new UserLogIn
+        {
+            UserId = testUserId,
+            UserEmail = "test@user.com",
+            UserPwd = "hashedPassword",
+            UserStatus = "verified"
+        });
+        _context.UserDetails.Add(new UserDetails
+        {
+            UserId = testUserId,
+            UserFullName = "Test User",
+            DateOfBirth = DateTime.Now,
+            IsAdmin = false
+        });
+        // Add test admin to database
+        _context.UserLogIn.Add(new UserLogIn
+        {
+            UserId = testAdminId,
+            UserEmail = "admin@knithub.no",
+            UserPwd = "hashedPassword",
+            UserStatus = "verified"
+        });
+        _context.UserDetails.Add(new UserDetails
+        {
+            UserId = testAdminId,
+            UserFullName = "Admin User",
+            DateOfBirth = DateTime.Now,
+            IsAdmin = true
+        });
+
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        // Delete the created PDF file
+        var recipeFilePath = Path.Combine(_mockStoragePath, $"{testRecipeId}.pdf");
+        if (File.Exists(recipeFilePath))
+        {
+            File.Delete(recipeFilePath);
+        }
+
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+    // Helper method to create a mock form file
+    private Mock<IFormFile> CreateMockFormFile(string fileName, string contentType, byte[] content)
+    {
+        // Create a mock form file
+        var fileMock = new Mock<IFormFile>();
+        var ms = new MemoryStream(content);
+        ms.Position = 0; 
+
+        // Set up the mock behavior
+        fileMock.Setup(_ => _.OpenReadStream()).Returns(ms);
+        fileMock.Setup(_ => _.FileName).Returns(fileName);
+        fileMock.Setup(_ => _.Length).Returns(ms.Length);
+        fileMock.Setup(_ => _.ContentType).Returns(contentType);
+
+        return fileMock;
+    }
+
+    [Fact]
+    public void UploadRecipe_Ok()
+    {
+        // Set up test data
+        var mockFile = CreateMockFormFile("test.pdf", "application/pdf", Encoding.UTF8.GetBytes("Dummy PDF content"));
+        var formData = new RecipeController.FormData
+        {
+            UserToken = "userToken",
+            RecipeName = "New Recipe",
+            NeedleSize = 5,
+            KnittingGauge = "10x10",
+            Notes = "Test notes",
+            RecipeFile = mockFile.Object
+        };
+
+        // Call controller and verify success
+        var result = _controller.UploadRecipe(formData);
+        Assert.IsType<CreatedResult>(result);
+    }
+
+    [Fact]
+    public void BadReuqstUpload_Fails()
+    {
+        // Set up test data
+        var request = new RecipeController.FormData{};
+
+        // Call controller and verify failure
+        var result = _controller.UploadRecipe(request);
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+    
+    [Fact]
+    public void FakeTokenUpload_Fails()
+    {
+        // Set up test data
+        var mockFile = CreateMockFormFile("test.pdf", "application/pdf", Encoding.UTF8.GetBytes("Dummy PDF content"));
+        var formData = new RecipeController.FormData
+        {
+            UserToken = "fakeToken",
+            RecipeName = "New Recipe",
+            NeedleSize = 5,
+            KnittingGauge = "10x10",
+            Notes = "Test notes",
+            RecipeFile = mockFile.Object
+        };
+
+        // Call controller and verify failure
+        var result = _controller.UploadRecipe(formData);
+        var response = Assert.IsType<ObjectResult>(result);
+        Assert.Equal(422, response.StatusCode);
+    }
+
+}
