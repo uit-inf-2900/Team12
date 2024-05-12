9b4a40fdc0c3631305c2aac98f2b4faecb801de4
Emilie Steen <est176@uit.no>
2024-05-12 20:16:49 +0200

    commented home page

--- front/src/pages/Home/home.js
+++ front/src/pages/Home/home.js
@@ -2,18 +2,15 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import StatisticBox from './StatisticBox';
 import { useNavigate } from 'react-router-dom';
-
-// Import images
 import { getImageByName } from '../../images/getImageByName';
-
-// Css styles
 import "../../GlobalStyles/main.css";
 import "../about/about.css";
 import './StatisticBox.css';
 import InstagramFeed from '../../Components/UI/InstagramFeed'; 
 
-
-
+/**
+ * The home page with inventory statistics, project states and instragramfeed.
+ */
 export const Home = () => {
   const navigate = useNavigate();
   const [userProfileState, setUserProfileState] = useState({ userFullName: '', userEmail: '' });
@@ -22,7 +19,7 @@ export const Home = () => {
   const [completeProjects, setCompleteProjects] = useState(0);
   const [ongoingProjects, setOngoingProjects] = useState(0);
 
-  // access to instagram
+  // Access token to instagram
   const accessTokenInsta = 'IGQWRNYjdRX3BnVHFmdVR0Qm5yR3RDWml0TTgwc3lhV1VRZAmw5U3I2eWZAkUTRKekRzOS1JWEt5REEzZA3JHX0dDSXVfdVpodWlHRXFLbngwdEtSVXhuaXdtYmRSY0dGSzhvR1NVQkhnMmlJSE5JNHFmMFJCMS1IdjAZD';
   
   // Function to handle navigation
@@ -33,6 +30,7 @@ export const Home = () => {
 
   useEffect(() => {
     const token = sessionStorage.getItem('token');
+
     // Fetches the name of the user
     if (token) {
       const address = 'http://localhost:5002/getprofileinfo?userToken=' + token;
@@ -43,7 +41,8 @@ export const Home = () => {
         .catch(error => {
           console.error("Error fetching profile data: ", error);
         });
-        
+      
+      // Fetches project and inventory statistics
       const fetchStatistic = async () => {
         try {
           const baseAddress = "http://localhost:5002";
@@ -51,18 +50,18 @@ export const Home = () => {
           const data = response.data;
           console.log(data);
 
+          // Find yarn inventory
           if(data.yarnInventory){
             setYarnInventoryLength(data.yarnInventory.length);
           }
+          // Find needle inventory
           if(data.needleInventory){
             setNeedleInventoryLength(data.needleInventory.length);
           }
             
-          // Fetches the number of complete projects
           const finishedProjectsResponse = await axios.get(`${baseAddress}/getcompleteprojects?userToken=${token}`);
           setCompleteProjects(finishedProjectsResponse.data.completeProjects);
     
-          // Fetches the number of ongoing projects 
           const ongoingProjectsResponse = await axios.get(`${baseAddress}/getongoingprojects?userToken=${token}`);
           setOngoingProjects(ongoingProjectsResponse.data.ongoingProjects);
         } catch (error) {
@@ -72,12 +71,13 @@ export const Home = () => {
       fetchStatistic();
       }
   }, []);
-    
-
+  
+  // Home page
   return (
     <div className='page-container'>
       <h1 style={{padding: '20px'}}>Hello, good to see you {userProfileState.userFullName || ''}!</h1>
       <div className="home-container" style={{'display': 'flex', padding: '20px', alignItems:'flex-start'}}>
+        {/* Show project and inventory */}
         <div className="statistics-container" style={{width:'40%', alignSself: 'flex-start'}}>
           <h3>Here is an overview of your projects and inventory:</h3>
           <div className="StatisticBox" style={{ display: 'flex', flexWrap: 'wrap', justifyContent:'center' }}> 
@@ -87,13 +87,13 @@ export const Home = () => {
             <StatisticBox icon={getImageByName('openBook')} label="ongoing projects" value={ongoingProjects.toString()} onClick={() => handleNavigate('/projects', 1)}/>
           </div>
         </div>
+        {/* Show image on the right side of home page */}
         <div className="creative-content-container" style={{width:'60%', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column'}}>
           <h3>Today is the day to be creative! </h3>
           <img src={getImageByName('huggingYarn')} style={{ alignItems: 'center'}} alt="Pile of sweaters"/>
         </div>
       </div>
-     
-
+      {/* Show instagram feed */}
       <h2  style={{padding: '20px', paddingTop:'30px'}}>Here are some inspiration from Instagram </h2>
       <InstagramFeed accessToken={accessTokenInsta} />
     </div>
