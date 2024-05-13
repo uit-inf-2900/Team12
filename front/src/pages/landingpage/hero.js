6b57d8386513d61208442a8fbb50caf7a569bf92
Marie Alette Stenhaug <mst301@uit.no>
2024-05-13 18:44:21 +0200

    change branch

---
+++ front/src/pages/landingpage/hero.js
@@ -0,0 +1,23 @@
+import {react} from 'react';
+import heroImg from '../../images/logo/logoOrange.svg'
+
+
+
+
+
+const Hero = ({}) => {
+
+    return(
+
+        <div>
+            <img>  </img>
+
+
+        </div>
+
+    );
+
+}
+
+
+export default Hero;
